ed96c28d1566834c52f110c5e1d8627c769b6a46
shsv382 <shsv382@gmail.com>
2017-11-18 14:12:06 +0400

    20

--- sections/script.js
+++ sections/script.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
-	$(".head-banner > h3").fadeIn(600, function(){
-		$(".head-banner > h4").fadeIn(600, function(){
-			$(".head-banner > h1").fadeIn(600);
-		});
-	});
+	head_banner();
+	img_change();
 	$(".show-menu-mob").on("click", function(){
 		show_nav();
 	});
@@ -11,4 +8,22 @@ $(document).ready(function(){
 
 function show_nav() {
 	$(".nav-desktop").slideToggle(300);
+}
+
+function head_banner() {
+	$(".head-banner > h1").fadeIn(600);
+}
+
+function img_change() {
+	$("head-banner-image").css({opacity: 0.0});
+	$("image-1").animate({opacity: 1.0}, 100);
+		var i = 0;
+		var timerId = setInterval(function() {
+			$(".image-" + (i%5 + 1)).animate({opacity: 1.0}, 1000);
+			var past = i%5 == 0 ? $(".image-5") : $(".image-" + (i%5));
+			past.animate({opacity: 0.0}, 1000);
+			i++;
+			hb.style.backgroundSize = "cover";
+		}, 5000);
+	
 }
