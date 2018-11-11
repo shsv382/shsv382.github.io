fa3b595425b91eaff5def39ede6a094ce6dc843d
shsv382 <shsv382@gmail.com>
2018-11-11 18:32:32 +0300

    Added huddle

---
+++ huddle/precache-manifest.62f0ff6a623046014bebdeeea64af5bf.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "7940efc40d8e3b477e16cc41b0287139",
+    "url": "/huddle/static/media/Poppins-Bold.7940efc4.ttf"
+  },
+  {
+    "revision": "042793c8268a36ef87d8",
+    "url": "/huddle/static/css/main.b8775b1d.chunk.css"
+  },
+  {
+    "revision": "9400b37f0fa0f48e8f4b",
+    "url": "/huddle/static/js/1.9400b37f.chunk.js"
+  },
+  {
+    "revision": "429adb545bb690e08811",
+    "url": "/huddle/static/js/runtime~main.429adb54.js"
+  },
+  {
+    "revision": "c90d96c2492a2d6f0863256f2bf9015d",
+    "url": "/huddle/static/media/icon-email.c90d96c2.svg"
+  },
+  {
+    "revision": "4c8c2ba2f1cc804f9092d504c9b0decd",
+    "url": "/huddle/static/media/icon-phone.4c8c2ba2.svg"
+  },
+  {
+    "revision": "3bd921c0d2fac634b2cff66402bb074c",
+    "url": "/huddle/static/media/bg-section-bottom-desktop-1.3bd921c0.svg"
+  },
+  {
+    "revision": "6a2a7d2b0d75606c69d2444da37844d4",
+    "url": "/huddle/static/media/bg-footer-top-desktop.6a2a7d2b.svg"
+  },
+  {
+    "revision": "bd9436af597514dd65ca5d639f1858ba",
+    "url": "/huddle/static/media/bg-section-top-desktop-1.bd9436af.svg"
+  },
+  {
+    "revision": "8acafd86a2f5b53735ef9f2a6bb01c21",
+    "url": "/huddle/static/media/bg-section-top-mobile-1.8acafd86.svg"
+  },
+  {
+    "revision": "6ef9dec4bd60b1c2c7915108316f5d75",
+    "url": "/huddle/static/media/bg-section-bottom-mobile-1.6ef9dec4.svg"
+  },
+  {
+    "revision": "144da12e04f88d8d639f022bc8f997aa",
+    "url": "/huddle/static/media/bg-footer-top-mobile.144da12e.svg"
+  },
+  {
+    "revision": "042793c8268a36ef87d8",
+    "url": "/huddle/static/js/main.042793c8.chunk.js"
+  },
+  {
+    "revision": "e1c83f9474e0cc1d84a13c6d1ddf3ca5",
+    "url": "/huddle/static/media/OpenSans-SemiBold.e1c83f94.ttf"
+  },
+  {
+    "revision": "d7d5d4588a9f50c99264bc12e4892a7c",
+    "url": "/huddle/static/media/OpenSans-Regular.d7d5d458.ttf"
+  },
+  {
+    "revision": "f5331cb6372b6c0d8baf2dd7e200498c",
+    "url": "/huddle/static/media/OpenSans-Bold.f5331cb6.ttf"
+  },
+  {
+    "revision": "3cb867305e954abaa1a8b0dd7a4e7c06",
+    "url": "/huddle/static/media/logo.3cb86730.svg"
+  },
+  {
+    "revision": "88090acec5276438b135a468364edf12",
+    "url": "/huddle/static/media/screen-mockups.88090ace.svg"
+  },
+  {
+    "revision": "4918e1492526a96242981765fe4321ae",
+    "url": "/huddle/static/media/icon-communities.4918e149.svg"
+  },
+  {
+    "revision": "1203b299cf1910559663db88126d869e",
+    "url": "/huddle/static/media/icon-messages.1203b299.svg"
+  },
+  {
+    "revision": "6641e825f006d463db7028607e3ee994",
+    "url": "/huddle/static/media/illustration-flowing-conversations.6641e825.svg"
+  },
+  {
+    "revision": "bb27da81d1984faa5ef57668534ff1a5",
+    "url": "/huddle/static/media/illustration-grow-together.bb27da81.svg"
+  },
+  {
+    "revision": "8a5af9952fb5ed23779d589621b108a5",
+    "url": "/huddle/static/media/illustration-your-users.8a5af995.svg"
+  },
+  {
+    "revision": "ae87c3298995535de48252b066c6f5aa",
+    "url": "/huddle/static/media/logo-mono.ae87c329.svg"
+  },
+  {
+    "revision": "470da926b50f0587123d64c92d7a812d",
+    "url": "/huddle/index.html"
+  }
+];
