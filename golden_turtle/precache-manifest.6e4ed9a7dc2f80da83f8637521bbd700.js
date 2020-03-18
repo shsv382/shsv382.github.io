910284ad4fa54dcb93b2b08f322236970f1e0e84
shsv382 <shsv382@gmail.com>
2020-03-19 00:23:05 +0300

    Added GT

---
+++ golden_turtle/precache-manifest.6e4ed9a7dc2f80da83f8637521bbd700.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f3f1df63a5746521718497a7c55d6d4",
+    "url": "/golden_turtle/index.html"
+  },
+  {
+    "revision": "7c169fd724345ad06199",
+    "url": "/golden_turtle/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1bbba9f7c52cddf9abdb",
+    "url": "/golden_turtle/static/css/main.2d690103.chunk.css"
+  },
+  {
+    "revision": "7c169fd724345ad06199",
+    "url": "/golden_turtle/static/js/2.a1ad688d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/golden_turtle/static/js/2.a1ad688d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1bbba9f7c52cddf9abdb",
+    "url": "/golden_turtle/static/js/main.ca81e9b7.chunk.js"
+  },
+  {
+    "revision": "2783c09b9fa19176e962",
+    "url": "/golden_turtle/static/js/runtime-main.2cc1edec.js"
+  },
+  {
+    "revision": "664a5bfd5cffe79f0882e175165794af",
+    "url": "/golden_turtle/static/media/upload.664a5bfd.png"
+  }
+]);
