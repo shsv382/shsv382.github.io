caee195d9630a13a3797a8e5c665d80c93e35a84
shsv382 <shsv382@gmail.com>
2020-03-19 17:30:25 +0300

    Re-formed repo

---
+++ precache-manifest.efca0c3fc209d2984eecc083846dba56.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac71c4dc9dc8f91775d9286cabc0bd5",
+    "url": "/golden_turtle/index.html"
+  },
+  {
+    "revision": "7c169fd724345ad06199",
+    "url": "/golden_turtle/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fa7c5243397a2aac9113",
+    "url": "/golden_turtle/static/css/main.c9032ca8.chunk.css"
+  },
+  {
+    "revision": "7c169fd724345ad06199",
+    "url": "/golden_turtle/static/js/2.a1ad688d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/golden_turtle/static/js/2.a1ad688d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa7c5243397a2aac9113",
+    "url": "/golden_turtle/static/js/main.8c790d3f.chunk.js"
+  },
+  {
+    "revision": "2783c09b9fa19176e962",
+    "url": "/golden_turtle/static/js/runtime-main.2cc1edec.js"
+  },
+  {
+    "revision": "664a5bfd5cffe79f0882e175165794af",
+    "url": "/golden_turtle/static/media/upload.664a5bfd.png"
+  }
+]);
